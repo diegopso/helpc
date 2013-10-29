34a0fa4421c66091d3c81716cbfae28c1e64a27e
Djonathas Cardoso <djonathascc@hotmail.com>
2013-10-29 19:09:47 -0300

    alterações do djonathas

--- app/views/_master/master.php
+++ app/views/_master/master.php
@@ -6,15 +6,18 @@
     <script src="~/jquery/jquery.min.js"></script>
     <script src="~/js/bootstrap.min.js"></script>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
 
             //Opções - alert(document.body.clientWidth); / alert(window.screen.width); / alert(screen.availWidth);
 
             //Comportamento do CSS quando a resolução do navegador for menor que 500px;
             if (document.body.clientWidth < 600) {
-                document.getElementById("imgCenter").className="resolImgCenter";
+                document.getElementById("imgCenter").className = "resolImgCenter";
             }
         });
+    </script>
+    <script type="text/javascript">
+        var ROOT = '<?= ROOT_VIRTUAL ?>';
     </script>
 </head>
 <body>
--- app/wwwroot/index.php
+++ app/wwwroot/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Copyright (c) 2011-2013, Valdirene da Cruz Neves Júnior <linkinsystem666@gmail.com>
  * All rights reserved.
