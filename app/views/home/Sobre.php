8df515420cf98202a274077251aa45ce8f362efc
Sergio Barros <sergyobarrus@hotmail.com>
2013-10-30 21:19:08 -0300

    ajuste commit

---
+++ app/views/home/Sobre.php
@@ -0,0 +1,50 @@
+<div class="bkgroundPage">
+    <div class=" container">
+        <div class="col-md-8 col-md-offset-2 section_header">
+            <h3>O que é</h3>
+            <p align="justify">A aplicação HelpC consiste em um sistema especialista que visa fornecer diagnósticos a usuários 
+                de computadores sobre possíveis problemas que podem estar presentes nas suas máquinas. Visando 
+                propor uma alternativa aos métodos convencionais de diagnósticos de problemas ocorridos em computadores, 
+                o HelpC consiste em fazer o mesmo procedimento, no entanto, utilizando técnicas da <abbr title="Inteligência Artificial" class="initialism"><b>IA</b></abbr> juntamente com 
+                outros recursos e tecnologias da informação, para apresentar um diagnóstico, mediante as informações 
+                previamente informada pelo usuário, que possa explicitar e informar ao mesmo, o real problema que 
+                ocorre com sua máquina, podendo tomar uma ação, mediante o resultado adquirido.</p>
+        </div>   
+
+        <div class="col-md-8 col-md-offset-2">
+            <h3>A Aplicação</h3>
+            <p align="justify">A Aplicação utiliza uma interface clara e objetiva, que consiste em contribuir em uma melhor experiência 
+                de uso do usuário com o sistema. Inicialmente, o diagnóstico se dá por meio de perguntas que são 
+                apresentadas ao usuário, no qual este interage inferindo suas respostas, ou seja, selecionando as 
+                opções exibidas pela aplicação de acordo com o seu conhecimento sobre o problema ocorrido. A aplicação 
+                se encarrega de cruzar as informações apresentadas pelo usuário com a sua base de conhecimento, que de 
+                acordo com essas informações tende a chegar em um resultado que retrate o real problema presente na 
+                máquina ou algo o mais próximo possível disto.</p>
+        </div>    
+        <div class="col-md-8 col-md-offset-2">
+            <h3>Como funciona</h3> 
+            <p align="justify">Este processo de diagnóstico se dá por meio de uma técnica da IA, conhecida como “sistemas especialistas”, 
+                que faz uso de um domínio específico (Diagnóstico de computadores), juntamente base de conhecimento 
+                aplicado a este domínio e demais tecnologias da informações. Juntos todos estes recursos operam a favor 
+                de um propósito: encontrar uma solução para uma real situação ou problema presente na máquina do usuário.</p>
+        </div>    
+        <div class="col-md-8 col-md-offset-2">
+            <p align="justify">A aplicação, posteriormente, irá atribuir características de inteligência, partindo do pressuposto que 
+                pode aprender uma solução, caso não esteja, ou que ainda não pertença a sua base de conhecimento, 
+                ou mesmo melhorar a eficácia do diagnóstico. Contudo, a aplicação também irá permitir a intervenção 
+                de usuário que tem conhecimento do domínio para incremento manual da base de conhecimento. Este 
+                comportamento por parte da aplicação, possibilita a maximização da base de conhecimento, logo também 
+                das soluções que podem ser apresentadas. Desta forma a aplicação estará sempre convergindo a uma ótima 
+                solução, consideração que caso a aplicação desconheça a solução baseado na informações cedidas, a HelpC 
+                captar os dados, levanta informações referente ao problema, cruza estas informações, propondo uma 
+                solução o que outrora poderá ser utilizada.</p>   
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+            <p align="justify">A aplicação HelpC, visa trazer comodidade e facilidade aos usuários que possuem interesse em conhecer 
+                ou obter maiores informações sobre determinado problema que possa está ocorrendo em sua máquina. 
+                Podendo o próprio usuário executar o reparo do problema, ou obter informações necessárias sobre o 
+                ocorrido com o computador, facilitando sua comunicação com profissional especializado na área de 
+                manutenção de computadores.</p> 
+        </div>
+    </div>
+</div>
