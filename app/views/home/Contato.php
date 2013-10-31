d64eb16a024bba084f1f6aec2a7e87b4a510c97f
Sergio Barros <sergyobarrus@hotmail.com>
2013-10-31 01:52:23 -0300

    Adicionado Pg Contato

---
+++ app/views/home/Contato.php
@@ -0,0 +1,20 @@
+<section class="is-features bkgroundPage">
+    <div id="main-wrapper" class="container">
+        <form method="post" action="">
+            <div class="pgCadastro">
+                <h3 class="form-signin-heading">Fale com a equipe HelpC</h3>
+                <input name="nome" id="login" type="text" class="form-control" placeholder="Infome seu Nome" autofocus="" required>
+                <br />
+                <input name="email" id="login" type="text" class="form-control" placeholder="informe seu E-mail" autofocus="" required>
+                <br />
+                <input name="assunto" id="login" type="text" class="form-control" placeholder="Informe o Assunto" autofocus="" required>
+                <br />
+                <input name="desetinatario" id="login" type="text" class="form-control" placeholder="destinatário" autofocus="" required>
+                <br />
+                <textarea name="mensagem" id="password" class="form-control" placeholder="Digite aqui sua mensagem" rows="6" required></textarea>
+                <br /> 
+                <button class="button" type="submit">Enviar</button>
+            </div>
+        </form>
+    </div>
+</section>
