0e69093ab3c4d63d8e70dbb1da599ca33044b333
Sergio Barros <sergyobarrus@hotmail.com>
2013-11-06 20:44:46 -0300

    ajuste paginas

--- app/views/help/desculpe.php
+++ app/views/help/desculpe.php
@@ -1,1 +1,9 @@
-<h1>Desculpe não podemos lhe ajudar</h1>
+<!--Pg. Desculpa-->
+<div class="container">
+    <div class="ajusteTopCenter">
+        <div>
+            <h2>Desculpe não podemos lhe ajudar</h2>
+            <img src="~/images/desculpe.png">
+        </div>
+    </div>
+</div>
